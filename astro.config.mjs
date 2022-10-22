1de3ef285804d61370830a6b8857418c4a827b78
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-10-22 11:00:40 -0700

    update firebase deploy set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://chenwarren.github.io',
-    base: '/solar-system-planets',
-});
+export default defineConfig();
